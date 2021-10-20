b552a348f45c8ab1e0963f451d360cf364c505e3
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-10-20 15:15:18 +0400

    Create EX7.BIOGRAPHY.cpp

---
+++ HOMEWORK/EX7.BIOGRAPHY.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	string name = "FizzaAli"; //used string to declare name
+	int age = 19;//used int to declare age
+	string hometown = "Islamabad";
+	cout << "Name: " << name << endl << "Age: " << age << endl << "Hometown: " << hometown << endl;
+	return 0;
+
+}
