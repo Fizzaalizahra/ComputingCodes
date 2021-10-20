f3cfec2f726ed0f88401e485889ff49e09f5e484
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-10-20 14:26:55 +0400

    Create EX1.INTRODUCTION.cpp

---
+++ HOMEWORK/EX1.INTRODUCTION.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Name: Fizza Ali" << endl; //print out name.
+    cout << "Age: 19" << endl; // print out age.
+    cout << "Current Address:  Ajman , UAE " << endl; // print out current home address. 
+    cout << "Permanent Address:  Islamabad, Pakistan " << endl; //print out permenant address.
+    return 0;
+}
+
