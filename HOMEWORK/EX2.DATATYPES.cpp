6242e29e03959288166fee3a91fd40d36a7bd1dd
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-10-20 14:27:10 +0400

    Create EX2.DATATYPES.cpp

---
+++ HOMEWORK/EX2.DATATYPES.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int  firstNumber = 1; //using int
+    cout << "firstNumber = " << firstNumber << endl;
+    bool iCanCode = true; // using boolean 
+    cout << "icancode = " << iCanCode << endl;
+    char hopefulGrade = 'a'; //using character
+    cout << "hopefulGrade = " << hopefulGrade << endl;
+    double myDecimal = 1.0f; //using double
+    cout << "myDecimal = " << myDecimal << endl;
+    string  minimalSentance = "y";// using string
+    cout << "minimalSentence = " << minimalSentance << endl;
+    int keyMash = 13213123; //using int
+    cout << "keyMash = " << keyMash << endl;
+    float  mysteryDataType = 5.6f; //Using float
+    cout << "mysteryDataType = " << mysteryDataType << endl;
+
+    return 0;
+}
+
