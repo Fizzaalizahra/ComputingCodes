6abcf3a78791aeff59d0442442247b33611437b7
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-10-20 14:27:16 +0400

    Create EX3.DIVISIONFIX.cpp

---
+++ HOMEWORK/EX3.DIVISIONFIX.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float numberOne = 50; //change interger to float to get answer in form of decimal. 
+	float numberTwo = 7;
+	cout << numberOne / numberTwo << endl;
+
+	return 0;
+
+}
