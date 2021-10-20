9c6f74ff3d4a8ccab1c933c6401a60a2e88ea318
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-10-20 15:02:09 +0400

    Create EX6.DECLARATION(SIMPLESUMS).cpp

---
+++ HOMEWORK/EX6.DECLARATION(SIMPLESUMS).cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x = 8; // initialising x = 8
+	int y = 10; // initialising y = 10
+	int sum = x + y; // initialising sum to be equal to x+y
+	cout << "Sum = " << sum << endl; // result
+	return 0;
+}
