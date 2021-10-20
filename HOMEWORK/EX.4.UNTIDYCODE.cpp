293992b05203f8bdb294088331a4e1c988e8e470
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-10-20 14:47:30 +0400

    Create EX.4.UNTIDYCODE.cpp

---
+++ HOMEWORK/EX.4.UNTIDYCODE.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int number = 6;
+    cout << "This is untidy code." << endl;
+    cout << "I'm surprised it works."; //deleted the gaps in between lines.
+    cout << "It has" << number << "lines of code- " << endl;
+    cout << "each more hideous than the last." << endl;  //put "<< endl;" at each line. 
+    cout << "You probably should add some line breaks in the text too" << endl;
+    return 0;
+
+
+ 
+
+
+  
+
+}
+ 
