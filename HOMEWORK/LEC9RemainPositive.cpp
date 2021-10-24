234de455b06951491a201c7d82ed5d659047aaa9
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-10-24 20:17:43 +0400

    Create LEC9RemainPositive.cpp

---
+++ HOMEWORK/LEC9RemainPositive.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float myNum = 20;//initializing myNum with float 
+
+	while (myNum > 0)
+	{
+		cout << myNum << endl;
+		myNum = myNum - 0.5;
+	}
+	return 0;
+}
+
+
+	
+
