4722d251bf2f95cb7d6f40857c2da857d88b9bc1
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-10-28 14:46:19 +0400

    Create LEC9LOOPY.cpp

---
+++ HOMEWORK/LEC9LOOPY.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>  
+using namespace std;
+int main() {
+
+	int myInt = 0, counter;
+	cout << "Enter a number" << endl;
+	cin >> counter;
+	do
+	{
+		cout << myInt << endl;
+		myInt++;
+
+	} while (myInt <= counter);
+
+}
