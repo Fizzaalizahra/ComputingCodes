85e8274b3dc0bd5ff9822a782d05e0d9b4406b38
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-10-28 14:46:07 +0400

    Create LEC9REVERSETIMESTABLE.cpp

---
+++ HOMEWORK/LEC9REVERSETIMESTABLE.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 108; //declare and initialise variable
+
+	while (num >= 9) //whileconditional check
+	{
+		cout << num << endl;
+		num = num - 9;
+	}
+	cin.get(); //keeps console window open in Visual Studio
+
+	return 0; 
+
+
+	
+}
+
+
+
