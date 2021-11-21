76f989aedf4e542509f051870574335a2e947d47
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-11-21 19:50:22 +0400

    Create LEC10Countdown.cpp

---
+++ HOMEWORK/LEC10Countdown.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+	for (int x = 10; x > 0 ; x--)
+	{
+		cout << x << endl; 
+
+		if (x==1)
+		
+			cout << " We have lift of "; 
+		return 0;
+
+		
+
+	}
+}
