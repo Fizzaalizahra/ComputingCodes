8367c16a36285582c641deebcc484e2fc21e52d7
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-11-25 16:02:26 +0400

    Create lec11arrays4.cpp

---
+++ HOMEWORK/lec11arrays4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int heartrates[8] = { 54 , 60 , 71 , 59, 62, 63, 60, 58 };
+	for (int i = 0; 1 < 8; i++) {
+		cout << heartrates[i] << endl;
+
+	}
+}
