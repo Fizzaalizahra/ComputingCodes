bfd4475696941ee71376cb6543b7fe0f2b6eab25
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-11-25 16:02:58 +0400

    Create LEC12advancing arrays.cpp

---
+++ HOMEWORK/LEC12advancing arrays.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	char letters[] = { 'C' , 'o', 'd', 'e', 'l', 'a', 'b' };
+	for (auto letters : letters) {
+
+		cout << letters;
+	}
+}
+
+
