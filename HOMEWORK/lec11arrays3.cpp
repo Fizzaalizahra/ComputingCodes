1179e4072cacb3a9617801ec49fd5c02f67f35f3
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-11-25 16:02:22 +0400

    Create lec11arrays3.cpp

---
+++ HOMEWORK/lec11arrays3.cpp
@@ -0,0 +1,8 @@
+#include<iosteam>
+using namespace std;
+int main()
+{
+	int heartrates[8] = { 54 , 60 , 71, 59, 62, 63, 60, 58 }; 
+	cout << heartrates[3] << endl;
+	cout << heartrates[6] << endl;
+}
