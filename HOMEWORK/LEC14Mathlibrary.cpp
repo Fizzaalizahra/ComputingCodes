17356ce9e524a77eea544be69d3daa1d535d720e
Fizza Ali <91000826+Fizzaalizahra@users.noreply.github.com>
2021-12-18 14:18:47 +0400

    Create LEC14Mathlibrary.cpp

---
+++ HOMEWORK/LEC14Mathlibrary.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <math.h>
+#include <array>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	cout << "The cube of 8 is " << pow(8, 3) << endl;//pow(number,exponent value)
+	cout << "The square-root of 8 is " << sqrt(8) << endl;//pow(number,exponent value)
+	cout << "The square-root of 8 is " << pow(8, 1 / 2) << endl;
+	cout << "The cube-root of 8 is " << cbrt(8) << endl;//pow(number,exponent value)
+	cout << "The cube-root of 8 is " << pow(8,1/2) << endl;
+	return 0; 
+}
